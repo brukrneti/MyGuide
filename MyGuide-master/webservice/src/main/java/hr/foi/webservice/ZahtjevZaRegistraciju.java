5ae2eb34c3e3c98d7b37024d439e201d8ca46c44
Bruno Krnetić <brukrneti@foi.hr>
2018-01-09 15:01:01 +0100

    Promjena nad webservisu za registraciju (dodana slika za profil korisnika)

--- MyGuide-master/webservice/src/main/java/hr/foi/webservice/ZahtjevZaRegistraciju.java
+++ MyGuide-master/webservice/src/main/java/hr/foi/webservice/ZahtjevZaRegistraciju.java
@@ -15,7 +15,7 @@ public class ZahtjevZaRegistraciju extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "http://yeloo.hr/AiR/MyGuideWebServices/Korisnik/register";
     private Map<String, String> params;
 
-    public ZahtjevZaRegistraciju(String ime, String prezime, String email, String korisnicko_ime, String lozinka, Integer tip_korisika, Response.Listener<String> listener){
+    public ZahtjevZaRegistraciju(String ime, String prezime, String email, String korisnicko_ime, String lozinka,String img_name, String img_path, Integer tip_korisika, Response.Listener<String> listener){
         super(Request.Method.POST, REGISTER_REQUEST_URL, listener,null);
         params = new HashMap<>();
         params.put("ime", ime);
@@ -23,6 +23,8 @@ public class ZahtjevZaRegistraciju extends StringRequest {
         params.put("email", email);
         params.put("korisnicko_ime",korisnicko_ime);
         params.put("lozinka", lozinka);
+        params.put("img_name",img_name);
+        params.put("img_path", img_path);
         params.put("id_tip_korisnika", tip_korisika + "");
     }
 
