bf4fd9b85b316a09b27e1bed7cc27aea8d276aea
Jurica Križanec <jura_kc@hotmail.com>
2018-01-21 08:19:51 +0100

    Promjene nad webservisom za uređenje profila

--- MyGuide-master/webservice/src/main/java/hr/foi/webservice/ZahtjevZaUredenjeProfila.java
+++ MyGuide-master/webservice/src/main/java/hr/foi/webservice/ZahtjevZaUredenjeProfila.java
@@ -15,12 +15,10 @@ public class ZahtjevZaUredenjeProfila extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "http://yeloo.hr/AiR/MyGuideWebServices/Korisnik/editProfile";
     private Map<String, String> params;
 
-    public ZahtjevZaUredenjeProfila(Integer id_korisnik, String korisnicko_ime, String lozinka, String ime, String prezime, String img_name, String img_path,Response.Listener<String> listener){
+    public ZahtjevZaUredenjeProfila(Integer id_korisnik, String ime, String prezime, String img_name, String img_path,Response.Listener<String> listener){
         super(Request.Method.POST, REGISTER_REQUEST_URL, listener,null);
         params = new HashMap<>();
         params.put("id_korisnik", id_korisnik +"");
-        params.put("korisnicko_ime", korisnicko_ime);
-        params.put("lozinka",lozinka);
         params.put("ime",ime);
         params.put("prezime", prezime);
         params.put("img_name",img_name);
