1d7f47d9ecfcdc7b01423f17edf1240ff8be5df7
Jurica Križanec <jura_kc@hotmail.com>
2018-01-22 12:43:26 +0100

    Promjene na adapteru za termine

--- MyGuide-master/app/src/main/java/hr/foi/myguide/Adapters/TerminAdapter.java
+++ MyGuide-master/app/src/main/java/hr/foi/myguide/Adapters/TerminAdapter.java
@@ -3,26 +3,124 @@ package hr.foi.myguide.Adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import hr.foi.database.entities.Termin;
-import hr.foi.database.entities.Tour;
 
-/**
- * Created by Mateo on 21.1.2018..
- */
+import hr.foi.myguide.R;
+import hr.foi.myguide.TourSchedule;
 
-public class TerminAdapter {
+public class TerminAdapter extends RecyclerView.Adapter<TerminAdapter.TerminViewHolder> {
     private Context mCtx;
     private List<Termin> terminList;
-    private TourAdapterListener tourAdapterListener;
+    private TerminAdapterListener terminAdapterListener;
 
     public TerminAdapter(Context mCtx, List<Termin> terminList) {
         this.mCtx = mCtx;
         this.terminList = terminList;
+    }
+    public void updateTerminsList(List<Termin> updatedList) {
+        terminList.clear();
+        terminList.addAll(updatedList);
+        this.notifyDataSetChanged();
+    }
+
+    public void setListener(TerminAdapterListener listener) {
+        this.terminAdapterListener = listener;
+    }
+
+    @Override
+    public TerminAdapter.TerminViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.list_tour_schedule_layout, null);
+        return new TerminViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TerminAdapter.TerminViewHolder holder, final int position) {
+        Termin termin = terminList.get(position);
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-d H:m:s");
+//        sdf.format(calendar.getTimeInMillis());
+
+        holder.terminIdTextView.setText(termin.getId_termin().toString());
+        holder.tourStartTextView.setText(termin.getVrijeme_od());
+        holder.tourEndTextView.setText(termin.getVrijeme_do());
+        holder.meetingPointTextView.setText(termin.getAdresa());
+        holder.noteTextView.setText(termin.getNapomena());
+
+
+        holder.tourScheduleListLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                terminAdapterListener.itemClicked(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return terminList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+        //public Item currentItem;
+
+        public ViewHolder(View v) {
+            super(v);
+            view = v;
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // item clicked
+                }
+            });
+        }
+    }
+
+    class TerminViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tourStartTextView, tourEndTextView, meetingPointTextView, noteTextView, terminIdTextView;
+        LinearLayout tourScheduleListLayout;
+
+        public TerminViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            terminIdTextView = itemView.findViewById(R.id.terminIdTextView);
+            tourStartTextView = itemView.findViewById(R.id.tourStartTextView);
+            tourEndTextView = itemView.findViewById(R.id.tourEndTextView);
+            meetingPointTextView = itemView.findViewById(R.id.meetingPointTextView);
+            noteTextView = itemView.findViewById(R.id.noteTextView);
+
+            tourScheduleListLayout = itemView.findViewById(R.id.id_tourSchedule_layout);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Integer terminId = Integer.parseInt(terminIdTextView.getText().toString());
+            Termin instanceToEdit;
+
+            for (int i = 0; i < terminList.size(); i++) {
+                if (terminList.get(i).id_termin == terminId) {
+                    instanceToEdit = terminList.get(i);
+                    Intent intent = new Intent(mCtx, TourSchedule.class);
+                    intent.putExtra("terminToEdit", instanceToEdit);
+                    mCtx.startActivity(intent);
+                }
+            }
+        }
     }
 }
