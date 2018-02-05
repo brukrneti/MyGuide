534ce655cf239139f289fadeafd70589b0f7c7a3
josdudas <josdudas@foi.hr>
2018-02-05 17:20:56 +0100

    Ispravak registracijskog testa i testa pocetne stranice

--- MyGuide-master/app/src/androidTest/java/hr/foi/myguide/PocetnaStranicaUITest.java
+++ MyGuide-master/app/src/androidTest/java/hr/foi/myguide/PocetnaStranicaUITest.java
@@ -1,13 +1,22 @@
 package hr.foi.myguide;
 
+import android.os.SystemClock;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import hr.foi.webservice.ZahtjevZaTuru;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.longClick;
@@ -35,15 +44,39 @@ public class PocetnaStranicaUITest {
 
     @Test
     public void buttonIsNotSelectable() {
+        SystemClock.sleep(1000);
         onView(withId(R.id. recyclerView)).check(matches(not(isSelected())));
     }
 
     @Test
     public void buttonClicked() {
+        SystemClock.sleep(1000);
         onView(withId(R.id.recyclerView)).check(matches((isDisplayed())));
-        onView(withId(R.id.recyclerView))
-                .perform(RecyclerViewActions.actionOnItem(
-                        hasDescendant(withText("Varazdin")), longClick()));
+        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Varazdin")), longClick()));
+    }
+
+    @Test
+    public void webService(){
+        SystemClock.sleep(1000);
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONObject dataJSON = jsonResponse.getJSONObject("data");
+                    boolean success = dataJSON.getBoolean("success");
+                    Integer idUserType = 1;
+                    if (success) {
+                    } else {
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        ZahtjevZaTuru zahtjevZaTuru = new ZahtjevZaTuru(responseListener);
+        RequestQueue queue = Volley.newRequestQueue(activityActivityTestRule.getActivity());
+        queue.add(zahtjevZaTuru);
     }
 
 }
--- MyGuide-master/app/src/test/java/hr/foi/myguide/RegistracijaTest.java
+++ MyGuide-master/app/src/test/java/hr/foi/myguide/RegistracijaTest.java
@@ -1,18 +1,15 @@
 package hr.foi.myguide;
 
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
+
 
 /**
  * Created by DudasPC on 02.02.2018..
  */
-@RunWith(MockitoJUnitRunner.class)
 public class RegistracijaTest {
 
     @Test
@@ -23,8 +20,8 @@ public class RegistracijaTest {
         when(registracija.checkEmail(email)).thenReturn(true);
         String email2 = "josdudas";
         when(registracija.checkEmail(email2)).thenReturn(false);
-        assertEquals(registracija.checkEmail(email), true);
-        assertEquals(registracija.checkEmail(email2), false);
+        assertEquals("Neispravan email",true, registracija.checkEmail(email));
+        assertEquals("Ispravan email", false, registracija.checkEmail(email2));
 
     }
 }
